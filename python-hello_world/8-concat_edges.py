52a1386686db68e0fe6b971c71eddc10825bd086
francomanzi <d.manzi1@alustudent.com>
2026-02-24 02:01:03 -0800

    corrections three

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str1 = "object-oriented"
-str2 = "programming with Python"
-print(str1 + str2)
+str1 = "Holberton"
+str2 = "School"
+print("Welcome to " + str1 + " " + str2 + "!")

23fc61dfc31dfb9697070e7e9029b402e17a60b3
francomanzi <d.manzi1@alustudent.com>
2026-02-24 02:05:03 -0800

    number eight

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-print("Welcome to " + str1 + " " + str2 + "!")
+print(chr(87)+chr(101)+chr(108)+chr(99)+chr(111)+chr(109)+chr(101)+chr(32)+chr(116)+chr(111)+chr(32)+str1+chr(32)+str2+chr(33))
